3c11c530114a189ca280c0e29714da70745a4e41
Ali Bahrami <110158421+AliBahramiprogrammer@users.noreply.github.com>
2024-07-16 21:15:36 +0330

    Update index.ts with cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -30,7 +30,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../../frontend/dist")))
-app.use();
+app.use(cors());
 
 app.use("/api/auth" , authRoutes)
 app.use("/api/properties", listingRoutes)
